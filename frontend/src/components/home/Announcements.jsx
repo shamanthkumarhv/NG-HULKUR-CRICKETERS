c05b33b500a534977cf0917253d5c3be8c5bd19a
Shamanth Kumar H V <shamanth.kumar.h.v@ibm.com>
2026-06-22 15:45:52 +0530

    Sprint 3.1: Modular homepage architecture error handling

---
+++ frontend/src/components/home/Announcements.jsx
@@ -0,0 +1,7 @@
+export default function Announcements() {
+  return (
+    <section>
+      <h2>Announcements</h2>
+    </section>
+  );
+}
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,21 +1,21 @@
 import HeroSection from "../../components/home/HeroSection";
 import PrizePool from "../../components/home/PrizePool";
-import TournamentDetails from "../../components/home/TournamentInfo";
+import TournamentInfo from "../../components/home/TournamentInfo";
 import Announcements from "../../components/home/Announcements";
 import Sponsors from "../../components/home/Sponsors";
-import GalleryPreview from "../../components/home/Gallery";
-import ContactSection from "../../components/home/Contact";
+import Gallery from "../../components/home/Gallery";
+import Contact from "../../components/home/Contact";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
       <PrizePool />
-      <TournamentDetails />
+      <TournamentInfo />
       <Announcements />
       <Sponsors />
-      <GalleryPreview />
-      <ContactSection />
+      <Gallery />
+      <Contact />
     </>
   );
 }
